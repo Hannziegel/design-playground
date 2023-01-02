8132a9446efe34f1f55de4793bae9b87dabed2cc
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2023-01-02 18:59:54 -0300

    Remove Files Add folders

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  <nav>
+    <a className="logo" href="./pages/home">
+      <img src="../assets/logo.svg" alt=""></img>
+    </a>
+    <a href="./pages/header">Header</a>
+  </nav>
+};
+
+export default Navbar;
