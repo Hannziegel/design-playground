de593297fae26627e6f2ca654f7b10a0eac82b07
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2023-04-28 16:43:36 -0300

    Update ThreeDimensionHoverIcon study

--- src/components/3DHoverIcon/ThreeDimensionHoverIcon.js
+++ src/components/3DHoverIcon/ThreeDimensionHoverIcon.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './ThreeDimensionHoverIcon.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faYoutube, faWhatsapp, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import VanillaTilt from 'vanilla-tilt';
 
 
 // import VanillaTilt from 'vanilla-tilt';
+function Tilt(props) {
+    const { options, ...rest } = props;
+    const tilt = useRef(null);
+  
+    useEffect(() => {
+      VanillaTilt.init(tilt.current, options);
+    }, [options]);
+  
+    return <div ref={tilt} {...rest} />;
+  }
+
 
 const ThreeDimensionHoverIcon = () => {
     const [backgroundColorHover, setBackgroundColorHover ] = useState("");
@@ -19,24 +31,43 @@ const ThreeDimensionHoverIcon = () => {
         setBackgroundColorHover('#fff');
     };
 
+    const options = {
+        max: 30,
+        speed: 400,
+        glare: true,
+        "max-glare": 1,
+    };
+
+
     return(
         <div className='socialMedia' style={{backgroundColor: backgroundColorHover}}>
             <ul className='sci'>
-                <li data-text="Facebook" onMouseEnter={() => handleMouseEnter("#1877f2")} onMouseLeave={() => handleMouseLeave()}>
-                    <a href='#' className='facebook'><FontAwesomeIcon icon={faFacebook} className='fa'/></a>
+                <li data-text="Facebook" onMouseEnter={() => handleMouseEnter("#1877f2")} onMouseLeave={() => handleMouseLeave()}>     
+                    <Tilt options={options}>
+                        <a href='#' className='facebook'><FontAwesomeIcon icon={faFacebook} className='fa'/></a>
+                    </Tilt>   
                 </li>
                 <li data-text="Twitter" onMouseEnter={() => handleMouseEnter("#0099ff")} onMouseLeave={() => handleMouseLeave()}>
-                    <a href='#' className='twitter'><FontAwesomeIcon icon={faTwitter} className='fa'/> </a>
+                    <Tilt options={options}>
+                            <a href='#' className='twitter'><FontAwesomeIcon icon={faTwitter} className='fa'/> </a>
+                    </Tilt>
                 </li>
                 <li data-text="Youtube" onMouseEnter={() => handleMouseEnter("#ff0000")} onMouseLeave={() => handleMouseLeave()}>
-                    <a href='#' className='youtube'><FontAwesomeIcon icon={faYoutube} className='fa'/> </a>
+                    <Tilt options={options}>
+                            <a href='#' className='youtube'><FontAwesomeIcon icon={faYoutube} className='fa'/> </a>
+                    </Tilt>
                 </li>
                 <li data-text="Instagram" onMouseEnter={() => handleMouseEnter("#e4405f")} onMouseLeave={() => handleMouseLeave()}>
-                    <a href='#' className='instagram'><FontAwesomeIcon icon={faInstagram} className='fa'/> </a>
+                    <Tilt options={options}>
+                            <a href='#' className='instagram'><FontAwesomeIcon icon={faInstagram} className='fa'/> </a>
+                    </Tilt>
                 </li>
                 <li data-text="Whatsapp" onMouseEnter={() => handleMouseEnter("#28c209")} onMouseLeave={() => handleMouseLeave()}>
-                    <a href='#' className='whatsapp'><FontAwesomeIcon icon={faWhatsapp} className='fa'/> </a>
+                    <Tilt options={options}>
+                            <a href='#' className='whatsapp'><FontAwesomeIcon icon={faWhatsapp} className='fa'/> </a>
+                    </Tilt>
                 </li>
+                
             </ul>
         </div>
     );
